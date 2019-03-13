9add3d34726b703ff1eb55a74691706f104e762f
Harold Espinoza <haroldesptru@gmail.com>
2019-03-13 10:27:23 -0600

    Importados los objetos de los modelos y la instancia de SQLAlchemy, importado y cargado objeto de configuracion dentro de la instancia principal de la app

--- books/app.py
+++ books/app.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template as render
+from config import Config
+from models import db, Category, Book
 
 app = Flask(__name__, static_folder="public", template_folder="views")
 
+app.config.from_object(Config)
+
 @app.route('/')
 def index():
-	return render("index.html")
+    c = Category(name="Hello")
+    print(c)
+    return render("index.html")
 
 if __name__ == "__main__":
-	app.run(port=8000, debug=True)
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
+    app.run(port=8000)
+
